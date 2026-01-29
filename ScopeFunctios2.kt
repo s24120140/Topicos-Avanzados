ba8e7c32e424ebec0ef06de613e22d84c14458ad
s24120140 <s24120140@alumnos.itsur.edu.mx>
2026-01-28 18:53:19 -0600

    Add user email update functionality

---
+++ ScopeFunctios2.kt
@@ -0,0 +1,12 @@
+data class User(val id: Int, var email: String)
+
+fun updateEmail(user: User, newEmail: String): User =  user.apply { this.email = newEmail }.also { println("Updating email for user with ID: ${it.id}") }
+
+fun main() {
+    val user = User(1, "old_email@example.com")
+    val updatedUser = updateEmail(user, "new_email@example.com")
+    // Updating email for user with ID: 1
+
+    println("Updated User: $updatedUser")
+    // Updated User: User(id=1, email=new_email@example.com)
+}
